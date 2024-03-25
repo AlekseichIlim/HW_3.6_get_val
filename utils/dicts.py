b1daab61bf17191a3070847cb9602997fd71bd5d
Alekseich <431410@mail.ru>
2024-03-25 11:11:47 +0800

    add code

--- utils/dicts.py
+++ utils/dicts.py
@@ -1,8 +1,10 @@
 coll = {'word1': 'dog', 'word2': 'cat'}
-def get_val(collection, key, default='fish'):
+def get_val(coll, key, default='fish'):
     for key_dict, values in coll.items():
         if key == key_dict:
             return values
 
     if key == '':
         return default
+
+print(get_val(coll, 'word2'))
